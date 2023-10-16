991869dc76d350734f31dbd1c2368a80d7eff242
Andrew Toussaint <ajntoussaint@gmail.com>
2023-10-16 08:28:31 -0500

    Set up database migrations

---
+++ EZFormsPrototype/Migrations/202310141605021_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿namespace EZFormsPrototype.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Field",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FormID = c.Int(nullable: false),
+                        Name = c.String(),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Form", t => t.FormID, cascadeDelete: true)
+                .Index(t => t.FormID);
+            
+            CreateTable(
+                "dbo.Form",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Field", "FormID", "dbo.Form");
+            DropIndex("dbo.Field", new[] { "FormID" });
+            DropTable("dbo.Form");
+            DropTable("dbo.Field");
+        }
+    }
+}
